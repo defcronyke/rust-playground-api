1cadcdcda5d50efb4eecf1ceeb108def1ea820dc
Jeremy Carter <jeremy@jeremycarter.ca>
2021-06-03 04:13:23 -0400

    Initial commit

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -sL -H 'Content-Type: application/json' -X POST -d '{"channel":"nightly","mode":"debug","edition":"2018","crateType":"lib","tests":true,"code":"fn main() {\n    println!(\"Hello, world!\");\n}","backtrace":false}' -H 'Referrer: https://play.rust-lang.org/?version=nightly&mode=debug&edition=2018&code=fn%20main()%20%7B%0A%20%20%20%20println!(%22Hello%2C%20world!%22)%3B%0A%7D' https://play.rust-lang.org/execute
+
