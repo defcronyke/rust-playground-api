0e24da921eb31aea13a9123c8c9395c330fc3efc
Jeremy Carter <jeremy@jeremycarter.ca>
2021-06-03 05:32:50 -0400

    Improve: build.sh, run.sh, test.sh, wasm.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Depends on the 'jq' tool.
 
-curl -sL -H 'Content-Type: application/json' -X POST -d '{"channel":"nightly","mode":"debug","edition":"2018","crateType":"lib","tests":true,"code":"fn main() {\n    println!(\"Hello, world!\");\n}","backtrace":false}' -H 'Referrer: https://play.rust-lang.org/?version=nightly&mode=debug&edition=2018&code=fn%20main()%20%7B%0A%20%20%20%20println!(%22Hello%2C%20world!%22)%3B%0A%7D' https://play.rust-lang.org/execute
+rust_playground_api_test() {
+  input_filename="$@"
 
+  if [ $# -eq 0 ]; then
+    input_str="fn main() {\n    println!(\"Hello, world!\");\n}"
+  else
+    input_str="$(cat $input_filename | sed ':a;N;$!ba;s/\n/\\n/g')"
+  fi
+
+  input_str2="$(printf '%s' "$input_str" | sed 's/\"/\\\"/g')"
+
+  input_param="$(printf '%b' "$input_str"|jq -sRr @uri)"
+
+  curl -sL -H 'Content-Type: application/json' -X POST -d '{"channel":"nightly","mode":"debug","edition":"2018","crateType":"lib","tests":true,"code":"'"$input_str2"'","backtrace":false}' -H "Referrer: https://play.rust-lang.org/?version=nightly&mode=debug&edition=2018&code=$input_param" https://play.rust-lang.org/execute
+}
+
+rust_playground_api_test "$@"
